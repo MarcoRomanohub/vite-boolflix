53cdd0bb7cedcd547b5522181129500efb83efc7
MarcoRomanoHub <marcoromano270@gmail.com>
2024-04-04 18:25:57 +0200

    [add] poster_path milestone 4

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -12,16 +12,25 @@ export default {
 
  <div class="col">
   <div class="card mb-3 ">
+    <img :src="`https://image.tmdb.org/t/p/w342` + cardObj.poster_path" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">{{ cardObj.title || cardObj.name }}</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary">{{ cardObj.original_title || cardObj.original_name }}</h6>
-      <p class="card-text">{{ cardObj.original_language }}</p>
+      <div v-if="cardObj.original_language!== 'it' & cardObj.original_language!== 'en' ">
+        <p class="card-text">{{ cardObj.original_language }}</p>
+      </div>
+      <div class="flag" v-else>
+        <img :src="'/public/flags/' +  cardObj.original_language + '.png'" alt="">
+      </div>
       <p class="card-text">{{ cardObj.vote_average}}</p>
-      <img :src="'/public/flags/' +  cardObj.original_language + '.png'" alt="">
     </div>
   </div>
 </div>
 
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .flag{
+    width: 80px;
+  }
+</style>
